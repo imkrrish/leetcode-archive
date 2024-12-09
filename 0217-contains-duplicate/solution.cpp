4d7f62bb3f241e4ce2241d290746e2ce36ea2462
Krishan Kumar <70051891+imkrrish@users.noreply.github.com>
2024-12-09 17:15:36 +0000

    Add 0217 Contains Duplicate (C++)

--- 0217-contains-duplicate/solution.cpp
+++ 0217-contains-duplicate/solution.cpp
@@ -1,17 +1,12 @@
-#include <unordered_map>
-
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, int> myMap;
-
-        for (int i = 0, n = nums.size(); i < nums.size(); i++) {
-            if (myMap.find(nums[i]) != myMap.end()) {
-                return 1;
-            } else {
-                myMap.insert({nums[i], i});
+        sort(nums.begin(), nums.end());
+        for (int i = 0, n = nums.size(); i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
             }
         }
-        return 0;
+        return false;
     };
 };
