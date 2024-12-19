91a42d671b3771bbdc21e830070285904bc5b825
Krishan Kumar <70051891+imkrrish@users.noreply.github.com>
2024-12-19 02:21:38 +0000

    Add 0049 Group Anagrams (C++)

--- 0049-group-anagrams/solution.cpp
+++ 0049-group-anagrams/solution.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> res{};
+        unordered_map<string, vector<string>> res;
 
-        for (const auto& word : strs) {
-            vector<int> freq(26, 0);
-
-            for (char c : word) {
-                freq[c - 'a']++;
-            }
-
-            string key = to_string(freq[0]);
-            for (int i = 1; i < 26; i++) {
-                key += ',' + to_string(freq[i]);
-            }
-
-            res[key].push_back(word);
+        for (auto& word : strs) {
+            string sorted_word = word;
+            std::sort(sorted_word.begin(), sorted_word.end());
+            res[sorted_word].push_back(word);
         }
 
         vector<vector<string>> result;
 
-        for (const auto& pair : res) {
+        for (auto& pair : res) {
             result.push_back(pair.second);
         }
 
         return result;
-    }
+    };
 };
