cf5c7079506c790aa86a412b611195c50ccdba21
Krishan Kumar <70051891+imkrrish@users.noreply.github.com>
2026-04-01 15:51:50 +0000

    Add 0342 Power of Four (C++)

--- 0342-power-of-four/solution.cpp
+++ 0342-power-of-four/solution.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
-    bool isPowerOfFour(double n) {
-        if (n == 1.0) {
+    bool isPowerOfFour(int n) {
+        if (n == 1) {
             return true;
-        } else if (n < 1) {
-            return false;
-        } else {
-            return isPowerOfFour(n / 4.0);
         }
+        if (n <= 0 || n % 4 != 0) {
+            return false;
+        }
+        return isPowerOfFour(n / 4);
     }
 };
