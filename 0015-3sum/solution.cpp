641408f6e6c933301756d5fa9e3722f4b824f6be
Krishan Kumar <70051891+imkrrish@users.noreply.github.com>
2026-05-15 18:23:59 +0000

    Add 0015 3Sum (C++)

---
+++ 0015-3sum/solution.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int target = -nums[i];
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int sum = nums[j] + nums[k];
+
+                if (sum == target) {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+
+                    while (j < k && nums[j] == nums[j + 1]) {
+                        j++;
+                    }
+
+                    while (j < k && nums[k] == nums[k - 1]) {
+                        k--;
+                    }
+
+                    j++;
+                    k--;
+                } else if (sum < target) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
