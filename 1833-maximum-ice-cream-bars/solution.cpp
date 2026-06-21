9f6dcd373eb5bfbcff272d7c5552d7aeed9daf36
Krishan Kumar <70051891+imkrrish@users.noreply.github.com>
2026-06-21 17:56:26 +0000

    Add 1833 Maximum Ice Cream Bars (C++)

---
+++ 1833-maximum-ice-cream-bars/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        sort(costs.begin(), costs.end());
+
+        int count = 0;
+
+        for (int& cost : costs) {
+            if (cost > coins) {
+                return count;
+            } else {
+                count++;
+                coins -= cost;
+            }
+        }
+
+        return count;
+    }
+};
